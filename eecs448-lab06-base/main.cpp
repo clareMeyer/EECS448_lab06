717d96172de28fed33c3d93751a0c7658577083c
cmeyer <artgirlie26@gmail.com>
2018-11-02 11:08:49 -0500

    added comments and list of bugs with descriptions*/

--- eecs448-lab06-base/main.cpp
+++ eecs448-lab06-base/main.cpp
@@ -11,11 +11,14 @@
 #include "LinkedListOfInts.h"
 
 TEST_CASE("Items can be added to the list", "[list]"){
+	/*testing if adding to the front works*/
 	SECTION("adding to the front"){
 		LinkedListOfInts listFront;
+		/*checing the size and the isEmtpy method*/
 		CHECK(listFront.size() == 0);
 		CHECK(listFront.isEmpty() == true);
 
+		/*adding nodes to the list*/
 		listFront.addFront(2);
 		CHECK(listFront.size() == 1);
 		/*this check is wrong, the list is not empty at this point*/
@@ -33,6 +36,7 @@ TEST_CASE("Items can be added to the list", "[list]"){
 		CHECK(vectorList[2] == 2);
 		CHECK(listFront.isEmpty() == false);
 	}
+	/*checking if the method addBack() works*/
 	SECTION("adding to the back"){
 		/*there is a bug here, adds to the front instead of to the back*/
 		LinkedListOfInts listBack;
@@ -46,7 +50,9 @@ TEST_CASE("Items can be added to the list", "[list]"){
 	}
 }
 
+/*testing the two remove methods()*/
 TEST_CASE("Items can be removed from the list", "[list]"){
+	/*checking removeFront() method*/
 	SECTION("removing from the front"){
 		/*this does not work at all, nothing is removed from the list, nothing changes*/
 		LinkedListOfInts listRF;
@@ -75,6 +81,7 @@ TEST_CASE("Items can be removed from the list", "[list]"){
 		vectorList = listRF.toVector();
 		CHECK(vectorList.size() == 0);
 	}
+	/*testing the removeBack() method*/
 	SECTION("removing from the back"){
 		/*this does not work at all, nothing is removed from the list, nothing changes*/
 		LinkedListOfInts listRB;
@@ -99,12 +106,12 @@ TEST_CASE("Items can be removed from the list", "[list]"){
 		CHECK(vectorList[0] == 0);
 		CHECK(vectorList.size() == 1);
 
-
 		listRB.removeBack();
 		vectorList = listRB.toVector();
 		CHECK(vectorList.size() == 0);
 	}
 }
+/*testing the search() method, should return true if found and false if not*/
 TEST_CASE("testing the search method", "[list]"){
 		LinkedListOfInts listS;
 		listS.addFront(100);
@@ -117,9 +124,11 @@ TEST_CASE("testing the search method", "[list]"){
 		CHECK(vectorList[1] == 1);
 		CHECK(vectorList[2] == 2);
 
+		/*checking if what was added is in the list*/
 		CHECK(listS.search(0) == true);
 		CHECK(listS.search(2) == true);
 		CHECK(listS.search(100) == true);
+		/*making sure it returns false if it is not in the list*/
 		CHECK(listS.search(10) == false);
 
 }
